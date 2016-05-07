0522632ce10a1a9f91059a21e6980c8424563180
JpnLavender <nagisa20000014@gmail.com>
2016-05-07 18:23:19 +0900

    internal commit

---
+++ app.rb
@@ -0,0 +1,8 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra/reloader' if development?
+require 'sinatra'
+require 'sinatra/json'
+require './models.rb'
+require 'open-uri'
+require 'time'
